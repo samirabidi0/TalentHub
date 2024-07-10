343842e29bc9aa0871e166055bb59c5f2621fc8b
samir <samirabidi1123@gmail.com>
2024-07-10 17:40:20 +0100

    style detailSell

--- frontend/app/Components/Common/Content/DetailArt/page.tsx
+++ frontend/app/Components/Common/Content/DetailArt/page.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 const page: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="card glass w-96">
-        <div className="card-body">
-          <h2 className="card-title text-center">Art</h2>
-          <p className="text-center">Details about Art.</p>
-        </div>
+    <div className="card card-side bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src="https://st2.depositphotos.com/3369547/8161/v/950/depositphotos_81611512-stock-illustration-web-developer-design.jpg"
+        className="w-20 h-20 mx-auto mb-2"
+        alt="Movie" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title">Samir</h2>
+      <p>Developpeur full-stuck</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">contact</button>
       </div>
     </div>
+  </div>
   );
 };
 export default page
--- frontend/app/Components/Common/Content/DetailSell/page.tsx
+++ frontend/app/Components/Common/Content/DetailSell/page.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 
 const page: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="card glass w-96">
-        <div className="card-body">
-          <h2 className="card-title text-center">Sell</h2>
-          <p className="text-center">Details about Marketing.</p>
-        </div>
-      </div>
+    <div className="block rounded-lg bg-white shadow-md dark:bg-surface-dark w-[350px] h-[300px]">
+    <a href="#!">
+      <img
+        className="rounded-t-lg w-full h-[250px]"
+        src="https://www.expedrion.biz/uploads/blog_featured_image/20230201084905__20230104092143__post6.jpg"
+        alt=""
+      />
+    </a>
+    <div className="p-4 text-surface dark:text-white">
+      <h5 className="mb-2 text-xl font-medium leading-tight">Card title</h5>
+      
+      <button
+        type="button"
+        className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-lg transition duration-150 ease-in-out hover:bg-primary-accent-300 focus:bg-primary-accent-300 focus:outline-none active:bg-primary-600 dark:shadow-black/30"
+      >
+        Button
+      </button>
     </div>
+  </div>
   );
 };
 export default page
