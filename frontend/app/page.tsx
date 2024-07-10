a7e9560c9547162ace078eb3f3d14ac5871c3267
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-10 22:54:57 +0200

    fixing the auto merge

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import HeroSection from './Common/HeroSection/page';
-
+import HeroSection from './Components/Common/HeroSection/page';
+import Link from 'next/link';
 
 const HomePage: React.FC = () => {
   return (
     <div className="w-full h-screen">
       <HeroSection />
+      <div className="flex justify-center mt-8">
+        <Link href="/Auth/SignUp">
+          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
+            Go to Auth Page
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
+
 export default HomePage;
