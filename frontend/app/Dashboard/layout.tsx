c842c7c5614761a7cca30ed4f379a3ca566dae46
Da7eddine <yassineboudidah@gmail.com>
2024-07-10 17:50:14 +0100

    Fixing structure

--- frontend/app/Dashboard/layout.tsx
+++ frontend/app/Dashboard/layout.tsx
@@ -1,7 +1,6 @@
-'use client';
 
 import { ReactNode } from 'react';
-import Sidebar from '../Components/Sidebar'
+
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -10,7 +9,6 @@ interface DashboardLayoutProps {
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="flex">
-      <Sidebar />
       <main className="flex-1 p-8">
         {children}
       </main>
