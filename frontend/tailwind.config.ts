390b5fc5f2cb7a686940fa6a931bd940a00edf46
samir <samirabidi1123@gmail.com>
2024-07-10 11:55:46 +0100

    new structure

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,9 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark", "cupcake"], // Add your preferred themes
-  },
+  plugins: [],
 };
 export default config;
