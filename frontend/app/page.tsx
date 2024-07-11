b629175674b764675ae9f68af165a4ade3b6a6f6
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-11 11:49:09 +0200

    enhance the look of the landing page and signup form done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,14 @@
+
 import React from 'react';
 import HeroSection from './Components/Common/HeroSection/page';
+import OurMission from './Components/Common/OurMission/page';
 import Link from 'next/link';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full">
       <HeroSection />
-      <div className="flex justify-center mt-8">
-        <Link href="/Auth/SignUp">
-          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
-            Go to Auth Page
-          </button>
-        </Link>
-      </div>
+      <OurMission />
     </div>
   );
 };
