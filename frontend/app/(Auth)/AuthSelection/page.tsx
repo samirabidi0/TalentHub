87c94d4d23611213ab919bcefbb92848939aa539
Da7eddine <yassineboudidah@gmail.com>
2024-07-12 17:52:50 +0100

    Edit profile

--- frontend/app/(Auth)/AuthSelection/page.tsx
+++ frontend/app/(Auth)/AuthSelection/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 interface AuthSelectionProps {
   role: string; //defining the type of the role 
